f0e234cc8b9c7646ae5f48e83062c7e3273036d6
haronYunis <haronyunis@icloud.com>
2018-05-06 02:10:10 -0700

    added .env file to hold geoCode api keys. Used browserify to run require in the browser. added package dotenv

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-function initMap() {
-  
+'use strict'
+require('dotenv').config();
+const key = process.env.key;
+
+geocode();
+function geocode() {
+  let location = '22 Main st Boston MA';
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+    params: {
+      address:location,
+      key:key
+    }
+  })
+  .then(function(response){
+    console.log(response);
+  })
+  .catch(function(error){
+    console.log(error);
+  })
 }
+
+
